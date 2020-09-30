a1fa0930bb89646c33d8f9e353ff910db49db6fe
Peter Rucker <peter@immunoconcepts.com>
2020-09-30 13:39:36 -0700

    Condensed all files into single python file.

--- src/Crawler.py
+++ src/Crawler.py
@@ -15,7 +15,7 @@ class Crawler(object):
         label = label.next_element
         label = label.next_element
         antigen = str(label.next_element)
-        antigen = antigen.replace("<td>","")
+        antigen = antigen.replace("<td>", "")
         antigen = antigen.replace("</td>", "")
         return antigen
 
@@ -23,45 +23,72 @@ class Crawler(object):
     def return_name(soup: BeautifulSoup) -> str:
         label = soup.find(class_="icap-titulo-all").contents
         name = str(label[2].replace("\r", "").strip())
-        return(name)
+        return name
 
-page = requests.get("https://anapatterns.org/view_pattern.php?pattern=")
-content = page.content
-soup = BeautifulSoup(content,'html.parser')
+    @staticmethod
+    def crawl_icap():
+        page = requests.get("https://anapatterns.org/view_pattern.php?pattern=")
+        content = page.content
+        soup = BeautifulSoup(content, 'html.parser')
+        tempfile = "temp.csv"
+        outputtable = [["Pattern Name", "Associated Antigens"]]
+        for i in range(30):
+            url = "https://anapatterns.org/view_pattern.php?pattern=" + str(i)
+            print(f"processing {url}")
+            page = requests.get(url)
+            content = page.content
+            soup = BeautifulSoup(content, 'html.parser')
+            outputtable.append([Crawler.return_name(soup), Crawler.return_antigen(soup)])
 
-outputtable = [["Pattern Name","Associated Antigens"]]
-for i in range(30):
-    url = "https://anapatterns.org/view_pattern.php?pattern=" + str(i)
-    print(f"processing {url}")
-    page = requests.get(url)
-    content = page.content
-    soup = BeautifulSoup(content,'html.parser')
-    outputtable.append([Crawler.return_name(soup),Crawler.return_antigen(soup)])
-
-f = open('temp.csv', 'w')
-for element in outputtable:
-    f.write("\t".join(element))
-    f.write("\n")
-f.close()
-
-
-
-try:
-    if filecmp.cmp("temp.csv","../data/ICAPoutputtable.csv",shallow=False):
-        print("No Changes since last pull")
-        os.remove("temp.csv")
-    else:
-        print("New information found, updating ICAPoutputtable.csv")
-        f = open('/data/ICAPoutputtable.csv', 'w')
+        f = open(tempfile, 'w')
         for element in outputtable:
             f.write("\t".join(element))
             f.write("\n")
         f.close()
-except:
-    print("First run detected, outputting new ICAPoutputtable.csv")
-    f = open('../data/ICAPoutputtable.csv', 'w')
-    for element in outputtable:
-        f.write("\t".join(element))
-        f.write("\n")
-    f.close()
-    os.remove("temp.csv")
+
+        try:
+            if filecmp.cmp(tempfile, "../data/ICAPoutputtable.csv", shallow=False):
+                print("No Changes since last pull")
+                os.remove(tempfile)
+            else:
+                print("New information found, updating ICAPoutputtable.csv")
+                f = open('/data/ICAPoutputtable.csv', 'w')
+                for element in outputtable:
+                    f.write("\t".join(element))
+                    f.write("\n")
+                f.close()
+        except FileNotFoundError:
+            print("First run detected, outputting new ICAPoutputtable.csv")
+            f = open('../data/ICAPoutputtable.csv', 'w')
+            for element in outputtable:
+                f.write("\t".join(element))
+                f.write("\n")
+            f.close()
+            os.remove("temp.csv")
+
+    @staticmethod
+    def tableorg():
+        data = []
+        with open("../data/ICAPoutputtable.csv") as file:
+            for line in file:
+                line = line.strip("\n")
+                lines = line.split("\t")
+                data.append(lines)
+        print(data)
+        outputdata = [['Associated Antigens', 'Pattern Name']]
+
+        for line in data[1:]:
+            for element in line[1].split(","):
+                outputdata.append([line[0], element])
+        for line in outputdata:
+            for pos, element in enumerate(line):
+                line[pos] = element.strip(" ")
+        print(outputdata)
+        f = open('../data/ICAPoutputtable2.csv', 'w')
+        for element in outputdata:
+            f.write("\t".join(element))
+            f.write("\n")
+        f.close()
+
+Crawler.crawl_icap()
+Crawler.tableorg()

8dcaea4e9728b85f35db7ce43d5896a577dab07d
Peter Rucker <peter@immunoconcepts.com>
2020-10-19 14:36:16 -0700

    Fixed directory bug. added info for synonyms, and table org now adds duplicate entry for each alternate name.

--- src/Crawler.py
+++ src/Crawler.py
@@ -1,7 +1,9 @@
 import requests
 import filecmp
 import os
+import re
 from bs4 import BeautifulSoup
+from typing import Optional
 
 class Crawler(object):
     @staticmethod
@@ -12,22 +14,53 @@ class Crawler(object):
         :return: string
         """
         label = soup.find("strong", text="Antigen Association")
-        label = label.next_element
-        label = label.next_element
+        label = label.next_element.next_element
         antigen = str(label.next_element)
-        antigen = antigen.replace("<td>", "")
-        antigen = antigen.replace("</td>", "")
+        antigen = antigen.replace("<td>", "").replace("</td>", "")
         return antigen
 
     @staticmethod
+    def return_previous_nomenclature(soup: BeautifulSoup) -> Optional[list]:
+        """
+        given a soup, will return a list of all previous nomenclature listed on current page, or None.
+        :param soup: parsed file from BeautifulSoup
+        :return: list, or None
+        """
+        label = soup.find("strong", text="Previous Nomenclature")
+        label = label.next_element.next_element
+        prevnom = str(label.next_element)
+        prevnom = prevnom.replace("<td>", "").replace("</td>", "")
+        if prevnom == "None":
+            return None
+        prevnomlist = re.split(", |/", prevnom)
+        return prevnomlist
+    @staticmethod
+    def return_description(soup: BeautifulSoup) -> str:
+        """
+        given a soup, will return the description listed on current page.
+        :param soup:
+        :return: str
+        """
+        label = soup.find("strong", text="Description")
+        label = label.next_element.next_element
+        descp = str(label.next_element)
+        descp = descp.replace("<td>", "").replace("</td>", "").strip()
+        return descp
+
+    @staticmethod
     def return_name(soup: BeautifulSoup) -> str:
+        """
+        given a soup, will return the name of the pattern.
+        :param soup:
+        :return: str
+        """
         label = soup.find(class_="icap-titulo-all").contents
         name = str(label[2].replace("\r", "").strip())
         return name
 
     @staticmethod
     def crawl_icap():
-        tempfile = "temp.csv"
+        tempfile = "temp.tsv"
         outputtable = [["Pattern Name", "Associated Antigens"]]
         for i in range(30):
             url = "https://anapatterns.org/view_pattern.php?pattern=" + str(i)
@@ -36,6 +69,10 @@ class Crawler(object):
             content = page.content
             soup = BeautifulSoup(content, 'html.parser')
             outputtable.append([Crawler.return_name(soup), Crawler.return_antigen(soup)])
+            if Crawler.return_previous_nomenclature(soup) != None:
+                prenomlist = Crawler.return_previous_nomenclature(soup)
+                for element in prenomlist:
+                    outputtable.append([element,Crawler.return_antigen(soup)])
 
         f = open(tempfile, 'w')
         for element in outputtable:
@@ -43,45 +80,46 @@ class Crawler(object):
             f.write("\n")
         f.close()
 
-        try:
-            if filecmp.cmp(tempfile, "../data/ICAPoutputtable.csv", shallow=False):
+
+        if os.path.isfile("data/ICAPoutputtable.tsv"):
+            if filecmp.cmp(tempfile, "data/ICAPoutputtable.tsv", shallow=False) is True:
                 print("No Changes since last pull")
                 os.remove(tempfile)
             else:
-                print("New information found, updating ICAPoutputtable.csv")
-                f = open('/data/ICAPoutputtable.csv', 'w')
+                print("New information found, updating ICAPoutputtable.tsv")
+                f = open('data/ICAPoutputtable.tsv', 'w')
                 for element in outputtable:
                     f.write("\t".join(element))
                     f.write("\n")
                 f.close()
-        except FileNotFoundError:
-            print("First run detected, outputting new ICAPoutputtable.csv")
-            if not os.path.exists('../data/'):
-                os.makedirs('../data')
-            f = open('../data/ICAPoutputtable.csv', 'w')
+        else:
+            print("First run detected, outputting new ICAPoutputtable.tsv")
+            if not os.path.exists('data'):
+                os.mkdir('data')
+            f = open('data/ICAPoutputtable.tsv', 'w')
             for element in outputtable:
                 f.write("\t".join(element))
                 f.write("\n")
             f.close()
-            os.remove("temp.csv")
+            os.remove(tempfile)
 
     @staticmethod
     def tableorg():
         data = []
-        with open("../data/ICAPoutputtable.csv") as file:
+        with open("data/ICAPoutputtable.tsv") as file:
             for line in file:
                 line = line.strip("\n")
                 lines = line.split("\t")
-                data.append(lines)
-        outputdata = [['Associated Antigens', 'Pattern Name']]
-
+                if lines[0][0:2] == "AC":
+                    data.append(lines[0:2])
+        outputdata = [['Pattern Name','Associated Antigens']]
         for line in data[1:]:
-            for element in line[1].split(","):
+            for element in re.split(", |/",line[1]):
                 outputdata.append([line[0], element])
         for line in outputdata:
             for pos, element in enumerate(line):
                 line[pos] = element.strip(" ")
-        f = open('../data/ICAPoutputtable2.csv', 'w')
+        f = open('data/ICAPoutputtable2.tsv', 'w')
         for element in outputdata:
             f.write("\t".join(element))
             f.write("\n")

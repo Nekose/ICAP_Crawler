9d24115bef414e8876bce951b816cd89759513f8
Peter Rucker <peter@immunoconcepts.com>
2020-10-21 15:30:21 -0700

    added changelog to track changes to files from most recent pull
    removed TODO

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from src.data_writer import Data_Writer
 dataset = []
 for i in range(5):
     dataset.append(Pattern('https://anapatterns.org/view_pattern.php?pattern=' + str(i)))
-print(Data_Writer.antigen_data_format(dataset))
-print(Data_Writer.pattern_data_format(dataset))
+Data_Writer.file_writer(Data_Writer.antigen_data_format(dataset),"data/antigen.tsv")
+Data_Writer.file_writer(Data_Writer.pattern_data_format(dataset),"data/pattern.tsv")
--- src/data_writer.py
+++ src/data_writer.py
@@ -1,5 +1,8 @@
 from src.Pattern import Pattern
 from typing import List
+import os
+import filecmp
+
 class Data_Writer(object):
 
     @staticmethod
@@ -26,3 +29,37 @@ class Data_Writer(object):
                 for altname in element.alt_names:
                     outputformat.append([altname,", ".join(element.antigens)])
         return outputformat
+
+    @staticmethod
+    def file_writer(outputformat: List[str],filename: str, tempfile: str = "tempfile.tsv") -> None:
+        f = open(tempfile, 'w')
+        for element in outputformat:
+            f.write("\t".join(element))
+            f.write("\n")
+        f.close()
+
+        if not os.path.isfile(filename):
+            print("first run detected")
+            os.rename(tempfile, filename)
+        elif Data_Writer.file_compare(tempfile,filename):
+            print("no new changes since last pull")
+            os.remove(tempfile)
+        else:
+            print("Changes since last pull found, writing changelog.txt and updating file")
+            with open(tempfile, 'r') as file1:
+                with open(filename, 'r') as file2:
+                    difference = set(file1).difference(file2)
+            difference.discard('\n')
+
+            with open("changelog.txt", 'a') as file_out:
+                for line in difference:
+                    file_out.write(line)
+            os.rename(tempfile, filename)
+
+    @staticmethod
+    def file_compare(newfile:str, previousfile: str) -> bool:
+        if filecmp.cmp(newfile, previousfile, shallow=False) is True:
+            return True
+        else:
+            return False
+
